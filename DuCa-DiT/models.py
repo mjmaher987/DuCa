18ec5d790d5f8352cf074bf0238b9b078df2f8e7
Alireza Shateri <alirezashateri7@gmail.com>
2025-04-10 10:57:54 -0400

    :hammer: chore: add an infinite loop to prevent program from exiting

--- DuCa-DiT/models.py
+++ DuCa-DiT/models.py
@@ -552,7 +552,7 @@ class DiT(nn.Module):
         # first_eps_nocache, second_eps_nocache = torch.split(eps_nocache, len(eps_nocache) // 2, dim=0)
         # first_eps_guide, second_eps_guide = torch.split(eps_guide, len(eps_guide) // 2, dim=0)
 
-        half_eps = eps_original + cfg_scale * (eps_original - eps_guide)
+        half_eps = eps_guide + cfg_scale * (eps_original - eps_guide)
         # half_eps = first_eps_nocache + cfg_scale * (first_eps_nocache - second_eps_guide) + cfg_scale * (first_eps_nocache - second_eps_nocache)
         eps = torch.cat([half_eps, half_eps], dim=0)
         return eps
