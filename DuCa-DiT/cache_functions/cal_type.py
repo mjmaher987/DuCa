3c732ce5869e73dc0849f7461bda8e33212bdd0c
Alireza Shateri <alirezashateri7@gmail.com>
2025-04-10 11:35:25 -0400

    update x_original in aggressive layer

--- DuCa-DiT/cache_functions/cal_type.py
+++ DuCa-DiT/cache_functions/cal_type.py
@@ -10,10 +10,10 @@ def cal_type(cache_dic, current):
     else:
         fresh_interval = cache_dic['fresh_threshold']
 
-    if (current['step'] % fresh_interval == 0) or first_step:
+    if (current['step'] % 6 == 0) or first_step:
         current['type'] = 'full'
         
-    elif ((current['step'] % fresh_interval) % 2 == 1): #[1,3,5] [2,4,6]
+    elif (current['step'] % 3 == 0): #[1,3,5] [2,4,6]
         current['type'] = 'ToCa'
 
     # 'aggressive' 'ToCa' 'FORA' 'Residual'
